8d52897e28b792cdaf922ea2506ee10a9a278929
Niferu <wargacki.patryq@gmail.com>
2023-01-29 20:48:45 +0100

    update

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -1,6 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-contract RandomIpfsNft {
-    // in progress...
+// Users will have to pay to mint NFT, and we as owners can withdraw balance
+// ChainLink VRF will pick random dog:
+// Pug -> epic
+// Shiba -> rare
+// St. Bernard -> common
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+
+contract RandomIpfsNft is VRFConsumerBaseV2 {
+    // Chainlink VRF Variables
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    mapping(uint256 => address) public s_requestIdToSender;
+
+    constructor(address vrfCoordinatorV2, uint64 subscriptionId, bytes32 gasLane, uint32 callbackGasLimit) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_subscriptionId = subscriptionId;
+        i_gasLane = gasLane;
+        i_callbackGasLimit = callbackGasLimit;
+    }
+
+    function requestNft() public returns (uint256 requestId) {
+        requestId = i_vrfCoordinator.requestRandomWords(i_gasLane, i_subscriptionId, REQUEST_CONFIRMATIONS, i_callbackGasLimit, NUM_WORDS);
+        s_requestIdToSender[requestId] = msg.sender;
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        // To prevent ChainLink node to be owner of "DOG" we do below with mapping.
+        // This way owner will be whoever called "requestNft()" function
+        address dogOwner = s_requestIdToSender[requestId];
+    }
+
+    function tokenURI(uint256) public {}
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -32,10 +32,16 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: "0.8.8",
+                version: "0.6.0",
             },
             {
                 version: "0.6.6",
+            },
+            {
+                version: "0.8.0",
+            },
+            {
+                version: "0.8.8",
             },
         ],
     },
