a8c2d53a07b07c983d333901c38145ed533410a6
Niferu <wargacki.patryq@gmail.com>
2023-02-13 14:49:02 +0100

    working to fix uploadToPinata function

--- utils/uploadToPinata.js
+++ utils/uploadToPinata.js
@@ -1,12 +1,35 @@
+// Fix in progres...
+
 const pinataSDK = require("@pinata/sdk")
 const path = require("path")
 const fs = require("fs")
+require("dotenv").config()
+
+const pinataApiKey = process.env.PINATA_API_KEY
+const pinataApiSecret = process.env.PINATA_API_SECRET
+const pinata = pinataSDK(pinataApiKey, pinataApiSecret)
 
 async function storeImages(imagesFilePath) {
     // Getting full path to images
     const fullImagesPath = path.resolve(imagesFilePath)
     const files = fs.readdirSync(fullImagesPath)
-    console.log(`Our Images: ${files}`)
+    console.log(`Our Images:`)
+    console.log(files)
+
+    let responses = []
+    console.log("----------------------------------------------------")
+    console.log("Uploading To Pinata...")
+    for (fileIndex in files) {
+        const readableStreamForFile = fs.createReadStream(`${fullImagesPath}/${files[fileIndex]}`)
+        try {
+            console.log(`Working On ${fileIndex}...`)
+            const response = await pinata.pinFileToIpfs(readableStreamForFile)
+            responses.push(response)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return { responses, files }
 }
 
 module.exports = { storeImages }
