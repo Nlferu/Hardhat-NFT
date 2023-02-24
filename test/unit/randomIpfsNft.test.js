e2c1278f483dcf878a5ee5776e7adb12fb221833
Niferu <wargacki.patryq@gmail.com>
2023-02-24 17:11:53 +0100

    1st test done

--- test/unit/randomIpfsNft.test.js
+++ test/unit/randomIpfsNft.test.js
@@ -5,10 +5,22 @@ const { developmentChains } = require("../../helper-hardhat-config")
 !developmentChains.includes(network.name)
     ? describe.skip
     : describe("Random IPFS NFT Unit Tests", function () {
-          beforeEach(async () => {})
+          let randomNft, deployer
+
+          beforeEach(async () => {
+              accounts = await ethers.getSigners()
+              deployer = accounts[0]
+              await deployments.fixture(["mocks", "randomnft"])
+              randomNft = await ethers.getContract("RandomIpfsNft")
+          })
           describe("Constructor", () => {
               it("Initializes the NFT Correctly.", async () => {
-                  // Check name, token and tokencounter
+                  const randomNftName = await randomNft.name()
+                  const randomNftSymbol = await randomNft.symbol()
+                  const randomNftCounter = await randomNft.getTokenCounter()
+                  assert.equal(randomNftName, "Random IPFS NFT")
+                  assert.equal(randomNftSymbol, "RIN")
+                  assert.equal(randomNftCounter.toString(), "0")
               })
           })
       })
