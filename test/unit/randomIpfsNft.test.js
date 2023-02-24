38c08f8351641b907480e8da92eedc87dff4adfd
Niferu <wargacki.patryq@gmail.com>
2023-02-24 15:53:29 +0100

    working on tests

--- test/unit/randomIpfsNft.test.js
+++ test/unit/randomIpfsNft.test.js
@@ -1,1 +1,14 @@
-// Tests in progress...
+const { assert } = require("chai")
+const { network, deployments, ethers } = require("hardhat")
+const { developmentChains } = require("../../helper-hardhat-config")
+
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Random IPFS NFT Unit Tests", function () {
+          beforeEach(async () => {})
+          describe("Constructor", () => {
+              it("Initializes the NFT Correctly.", async () => {
+                  // Check name, token and tokencounter
+              })
+          })
+      })
